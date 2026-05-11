ecf169cac11821afa83bf87954119cf901350d9e
UENO, M. <eunosium-1128@live.jp>
2026-05-11 11:49:24 +0900

    Refactor build script for Micropython installation

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,44 +1,16 @@
 #!/bin/bash
+set -exo pipefail
 
-set -ex
+sed -i.bak 's/gcc/$(GCC)/g' ${SRC_DIR}/py/mkenv.mk
+sed -i.bak 's/-Werror//g' ${SRC_DIR}/ports/unix/Makefile
 
-if [ `uname` != Darwin ]; then
-    sed -i 's/gcc/\$\(GCC\)/g' $SRC_DIR/py/mkenv.mk
-fi
+make -C ${SRC_DIR}/mpy-cross -j${CPU_COUNT} \
+    CFLAGS_EXTRA="${CFLAGS}" CPP="$CC -E"
 
-cd $SRC_DIR/lib
-sha=(
-    531cab9c278c947d268bd4c94ecab9153a961b43
-    e9de7e35f2339598b16cbb375f9992643ed81209
-    35aaec4418ad78628a3b935885dd189d41ce779b
-)
-library=(
-    axtls
-    libffi
-    berkeley-db-1.xx
-)
-name=(
-    micropython
-    atgreen
-    pfalcon
-)
-for i in ${!library[@]}; do
-    rmdir ${library[$i]}
-    wget https://github.com/${name[$i]}/${library[$i]}/archive/${sha[$i]}.zip
-    unzip ${sha[$i]}.zip
-    mv ${library[$i]}-${sha[$i]} ${library[$i]}
-done
+LDFLAGS_EXTRA=""
+[ "$(uname)" != "Darwin" ] && LDFLAGS_EXTRA="-lrt"
 
-cd $SRC_DIR/mpy-cross
-export CFLAGS_EXTRA="$CFLAGS"
-export CPP="$CC -E"
-make -j${CPU_COUNT}
+make -C ${SRC_DIR}/ports/unix -j${CPU_COUNT} V=1 \
+    CFLAGS_EXTRA="${CFLAGS}" LDFLAGS_EXTRA="${LDFLAGS_EXTRA}"
 
-cd $SRC_DIR/ports/unix
-if [ `uname` != Darwin ]; then
-    export LDFLAGS_EXTRA="-lrt"
-fi
-sed -ie 's/-Werror//g' Makefile
-make -j${CPU_COUNT} V=1
-
-mv micropython $PREFIX/bin
+install -m 755 ${SRC_DIR}/ports/unix/build-standard/micropython ${PREFIX}/bin/
