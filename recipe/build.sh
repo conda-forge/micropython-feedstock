90bf96f1896ac6767bb9c108998db434b65a47f1
UENO, M. <eunosium-1128@live.jp>
2026-05-11 17:55:20 +0900

    Simplify build.sh for Linux environment
    Refactor build script to simplify Linux checks and remove unnecessary exports.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 set -ex
 
-if [ "$(uname)" != "Darwin" ]; then
+export CFLAGS_EXTRA="$CFLAGS"
+
+if [ "$(uname)" == "Linux" ]; then
     sed -i.bak 's/gcc/$(GCC)/g' $SRC_DIR/py/mkenv.mk
 fi
-
 sed -i.bak 's/-Werror//g' $SRC_DIR/ports/unix/Makefile
 
-export CFLAGS_EXTRA="$CFLAGS"
-export CPP="$CC -E"
-
-make -C $SRC_DIR/mpy-cross -j${CPU_COUNT} \
-    BUILD="$SRC_DIR/mpy-cross/build"
-
-if [ "$(uname)" != "Darwin" ]; then
-    export LDFLAGS_EXTRA="-lrt"
-fi
+make -C $SRC_DIR/mpy-cross -j${CPU_COUNT} BUILD="$SRC_DIR/mpy-cross/build"
+make -C $SRC_DIR/ports/unix submodules -j${CPU_COUNT} V=1
 make -C $SRC_DIR/ports/unix -j${CPU_COUNT} V=1
-
 install -m 755 ${SRC_DIR}/ports/unix/build-standard/micropython ${PREFIX}/bin/
