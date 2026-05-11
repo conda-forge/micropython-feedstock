299726778b81ea79174567a1a1debeee867c54f7
UENO, M. <eunosium-1128@live.jp>
2026-05-11 18:21:37 +0900

    Update build script to install Micropython
    Change the make command to install Micropython instead of just building.

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,6 @@ fi
 sed -i.bak 's/-Werror//g' $SRC_DIR/ports/unix/Makefile
 
 make -C ${SRC_DIR}/mpy-cross -j${CPU_COUNT} V=1 BUILD="${SRC_DIR}/mpy-cross/build"
-make -C ${SRC_DIR}/ports/unix -j${CPU_COUNT} V=1 BUILD="${SRC_DIR}/ports/unix/build-standard"
+make -C ${SRC_DIR}/ports/unix install -j${CPU_COUNT} V=1 BUILD="${SRC_DIR}/ports/unix/build-standard" PREFIX=${PREFIX}
 mkdir -p ${PREFIX}/bin/
 install -m 755 ${SRC_DIR}/mpy-cross/build/mpy-cross ${PREFIX}/bin/
-install -m 755 ${SRC_DIR}/ports/unix/build-standard/micropython ${PREFIX}/bin/
