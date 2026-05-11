ed3bff5051a5e6c5bfdf94069ca1df51d9c2587e
UENO, M. <eunosium-1128@live.jp>
2026-05-11 12:03:56 +0900

    Refactor build.sh for improved OS compatibility
    Refactor build script to set environment variables and conditionally execute commands based on OS.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-set -exo pipefail
+set -ex
 
-sed -i.bak 's/gcc/$(GCC)/g' ${SRC_DIR}/py/mkenv.mk
-sed -i.bak 's/-Werror//g' ${SRC_DIR}/ports/unix/Makefile
+if [ "$(uname)" != "Darwin" ]; then
+    sed -i.bak 's/gcc/$(GCC)/g' $SRC_DIR/py/mkenv.mk
+fi
 
-make -C ${SRC_DIR}/mpy-cross -j${CPU_COUNT} \
-    CFLAGS_EXTRA="${CFLAGS}" CPP="$CC -E"
+sed -i.bak 's/-Werror//g' $SRC_DIR/ports/unix/Makefile
 
-LDFLAGS_EXTRA=""
-[ "$(uname)" != "Darwin" ] && LDFLAGS_EXTRA="-lrt"
+export CFLAGS_EXTRA="$CFLAGS"
+export CPP="$CC -E"
+make -C $SRC_DIR/mpy-cross -j${CPU_COUNT}
 
-make -C ${SRC_DIR}/ports/unix -j${CPU_COUNT} V=1 \
-    CFLAGS_EXTRA="${CFLAGS}" LDFLAGS_EXTRA="${LDFLAGS_EXTRA}"
+if [ "$(uname)" != "Darwin" ]; then
+    export LDFLAGS_EXTRA="-lrt"
+fi
+make -C $SRC_DIR/ports/unix -j${CPU_COUNT} V=1
 
 install -m 755 ${SRC_DIR}/ports/unix/build-standard/micropython ${PREFIX}/bin/
